ce5e9adfb751cee9a11f2a2794e108c47ad3eacd
Андрей Сутормн <ak.sutormin@gmail.com>
2021-09-17 16:49:36 +0300

    [Рефакторинг] Удаление лишних ссылок.

--- src/app/_services/address.service.ts
+++ src/app/_services/address.service.ts
@@ -3,13 +3,11 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { debounceTime, distinctUntilChanged, map } from "rxjs/operators";
 import { environment } from "src/environments/environment";
-import { AccountInfoLight } from "../_model/_input/account-info";
 import { AddressInfo } from "../_model/_input/address-info";
 
 @Injectable({ providedIn: 'root' })
 export class AddressService {
   constructor(private http: HttpClient) { }
-
   
   public getList(description: string): Observable<AddressInfo[]> {
     return this.http.get(`${environment.apiUrl}/addresses/list`, { params: { description: description } })
